87295c56ad4a74adb8db5ff6a901aef303d39714
John Backes <john.backes@rockwellcollins.com>
2015-04-07 11:07:20 -0500

    adding networking scripts

--- demo.cpp
+++ demo.cpp
@@ -79,7 +79,8 @@ int main(int argc, char * argv[])
   //for some reason we need to run the first command first and then this one
   //otherwise the pixy does not see any frames???
   pixy_command("runprogArg", 0x01, 8, 0x01, 1, END, &response, END);
-  pixy_command("cam_setAWB", 0x01, 1, 0, &response, 0);
+  //pixy_command("cc_setSigRegion", 0x01, 0, 0x01, 1, END, &response, END);
+  //pixy_command("cam_setAWB", 0x01, 1, 0, &response, 0);
   //pixy_command("runprog", 0x01, 8, END, &response, END);
   printf("sent running prog\n");
   //pixy_command("cam_setAWB", 0x01, 1, 0, &response, 0);
