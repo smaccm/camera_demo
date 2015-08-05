bc503bdb3f7476b4a6628b719539e4089ce8cc7a
Andrew Gacek <andrew.gacek@gmail.com>
2015-08-04 21:30:25 -0500

    Use cp/mv to prevent partial reads

--- attack.sh
+++ attack.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-cp skull.rgb attack.rgb
+# Copy, then move the file so it appears instantaneously to the
+# program. Otherwise, the program may do a partial read during the
+# copy.
+
+cp skull.rgb attack-load.rgb
+mv attack-load.rgb attack.rgb
+
