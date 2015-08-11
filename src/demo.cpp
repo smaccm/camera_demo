d5eb4962bdcee0faed76b9930d338e6fcd02ceb3
Andrew Gacek <andrew.gacek@gmail.com>
2015-08-11 01:40:16 -0400

    Repeatedly tell the Pixy to send video
    This seems to mitigate an issue where the Pixy would sometimes just
    stop sending data.

--- src/demo.cpp
+++ src/demo.cpp
@@ -87,14 +87,11 @@ int main(int argc, char * argv[])
   }
   
   printf("Starting camera\n");
-  int r = pixy_command("runprogArg", UINT8(8), UINT32(0), END, &response, END);
-  printf("Starting camera tracking (%d, %d)\n", r, response);
-  sleep(1);
-  r = pixy_command("runprogArg", UINT8(8), UINT32(1), END, &response, END);
-  printf("Camera tracking started (%d, %d)\n", r, response);
-  
-  int t = 0;
-  for(;;){
-    usleep(100000000);
+  pixy_command("runprogArg", UINT8(8), UINT32(0), END, &response, END);
+
+  printf("Starting camera tracking\n");
+  while (1) {
+    sleep(1);
+    pixy_command("runprogArg", UINT8(8), UINT32(1), END, &response, END);
   }
 }  
